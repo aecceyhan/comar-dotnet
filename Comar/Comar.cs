ef090771ff69a8e89f451fe4024fc81919620b70
Emre Ceyhan <a@emreceyhan.com>
2022-01-29 20:58:34 +0200

    first commit

---
+++ Comar/Comar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Comar
+{
+    public static class ComarMessageHalper
+    {
+        public static string ServiceURL { get; set; }
+        public static string SlackBotToken { get; set; }
+        public static string Authentication { get; set; }
+
+        public static bool Ping(SendMessageResource sendMessageResource)
+        {
+            var client = new RestClient($"{ServiceURL}/api/ping");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authentication", Authentication);
+
+            if (SlackBotToken is not null)
+                request.AddHeader("SlackBotToken", SlackBotToken);
+
+            var body = JsonSerializer.Serialize(sendMessageResource);
+            request.AddParameter("application/json", body,  ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            if (!response.IsSuccessful)
+                return false;
+
+            return true;
+        }
+    }
+
+    public class SendMessageResource
+    {
+        public string Channel { get; set; }
+        public string Message { get; set; }
+        public string BlockquoteColor { get; set;}
+        public List<string> AdditionalTexts { get; set; }
+
+    }
+    
+}
---
+++ Example/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Comar;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComarMessageHalper.Authentication = "123456";
+            ComarMessageHalper.ServiceURL = "http://comar-dev-elb-928123545.eu-central-1.elb.amazonaws.com";
+            //Also, you can set up Slack Bot Token when you initialise the request.
+            //ComarMessageHalper.SlackBotToken = "xb......";
+
+
+            AdditionalTextWithButtonsMesssageExample();
+
+        }
+
+        public static void BasicMesssageExample()
+        {
+            SendMessageResource sendMessageResource = new SendMessageResource
+            {
+                Channel = "#general",
+                Message = "Hello World"
+            };
+
+            var IsSuccessful = ComarMessageHalper.Ping(sendMessageResource);
+
+            if(IsSuccessful)
+                Console.WriteLine("Message sent successfully");
+        }
+
+        public static void BlockquoteMesssageExample()
+        {
+            SendMessageResource sendMessageResource = new SendMessageResource
+            {
+                Channel = "#general",
+                Message = "Hello World",
+                BlockquoteColor = "#00FF00"
+            };
+
+            var IsSuccessful = ComarMessageHalper.Ping(sendMessageResource);
+
+            if (IsSuccessful)
+                Console.WriteLine("Message sent successfully");
+        }
+
+        public static void AdditionalTextMesssageExample()
+        {
+            SendMessageResource sendMessageResource = new SendMessageResource
+            {
+                Channel = "#general",
+                Message = "Hello World",
+                BlockquoteColor = "#00FF00",
+                AdditionalTexts = new List<string>
+                {
+                    "This is an additional text",
+                }
+            };
+
+            var IsSuccessful = ComarMessageHalper.Ping(sendMessageResource);
+
+            if (IsSuccessful)
+                Console.WriteLine("Message sent successfully");
+        }
+
+        public static void AdditionalTextWithButtonsMesssageExample()
+        {
+            SendMessageResource sendMessageResource = new SendMessageResource
+            {
+                Channel = "#general",
+                Message = "Hello World",
+                BlockquoteColor = "#00FF00",
+                AdditionalTexts = new List<string>
+                {
+                    "This is an additional text",
+                    "<https://emreceyhan.com | click me>",
+                }
+            };
+
+            var IsSuccessful = ComarMessageHalper.Ping(sendMessageResource);
+
+            if (IsSuccessful)
+                Console.WriteLine("Message sent successfully");
+        }
+    }
+}
